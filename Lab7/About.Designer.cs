5dd9d60530a7398bfbe1f2b9ab24c2b16f1c20cc
D1mon4ik1 <128784535+D1mon4ik1@users.noreply.github.com>
2024-06-17 14:43:05 +0300

    lab7

---
+++ Lab7/About.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Lab7OOP
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            btnOK = new Button();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(173, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 20);
+            label2.TabIndex = 2;
+            label2.Text = "NotePadC#";
+            // 
+            // btnOK
+            // 
+            btnOK.Location = new Point(161, 174);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(119, 42);
+            btnOK.TabIndex = 3;
+            btnOK.Text = "ОК";
+            btnOK.UseVisualStyleBackColor = true;
+            btnOK.Click += btnOK_Click;
+            // 
+            // About
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(450, 228);
+            Controls.Add(btnOK);
+            Controls.Add(label2);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "About";
+            Text = "About NotePadC#";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label2;
+        private Button btnOK;
+    }
+}
